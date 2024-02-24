315e0f4738e48eed68d93facfce6cafe10755205
Andrew On <a_on93@hotmail.com>
2024-02-24 11:30:57 -0500

    added user login and registration

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,13 @@ ActiveRecord::Schema.define(version: 2021_06_26_062916) do
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
-
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
